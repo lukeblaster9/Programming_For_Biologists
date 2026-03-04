8ad3586a3aeb0bf9bf2282f2bfde9cf0739f8766
Luke Barnes <lb244@uark.edu>
2026-03-04 10:39:02 -0600

    Added a greetubg, comments, and math to script

--- Scripts/VSCode-Script.py
+++ Scripts/VSCode-Script.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
+# prompt the user for their name
+name = input("Please enter your name: ")
+
+# Two ways to print hello world
 print("hello world")
 print('hello world')
+
+# Print personalized greeting, using the name from input(above)
+print("hello,", name, "I hope you have  great day.")
+
+# some math
+print(7+7*9)
+print((7+7)*9)
