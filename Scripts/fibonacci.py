5c9856b3d125ba15e528c5fd5f4ba594832b2d15
Luke Barnes <lb244@uark.edu>
2026-04-01 10:11:54 -0500

    broke the code into functions, but they don't work yet

--- Scripts/fibonacci.py
+++ Scripts/fibonacci.py
@@ -2,29 +2,48 @@
 
 import argparse
 
-###----------------------accept and parse command line arguments
-# create an argument parser object
-parser = argparse.ArgumentParser(description="This script calculates the number at a given position \
-                                in the Fibonacci sequence")
 
-#add a positional argument, in this case, the position in the Fibonacci sequence
-parser.add_argument("position", help="Position in the Fibonacci sequence", type=int)
+###-------function to parse the command-line arguments
+def get_args():
+    ###----------------------accept and parse command line arguments
+    # create an argument parser object
+    parser = argparse.ArgumentParser(description="This script calculates the number at a given position \
+                                    in the Fibonacci sequence")
 
-# an optional argument for verbose output or not
-# if 'store_true', this means assign 'True' if the optional argument is specified
-# on the command line, so the default for store_true is actually false
-parser.add_argument("-v", "--verbose", help="Print verbose output", action='store_true')
-# parse the arguments
-args = parser.parse_args()
+    #add a positional argument, in this case, the position in the Fibonacci sequence
+    parser.add_argument("position", help="Position in the Fibonacci sequence", type=int)
 
-#initialize two integers
-a,b = 0,1
-for i in range(int(args.position)):
-    a,b = b, a+b
+    # an optional argument for verbose output or not
+    # if 'store_true', this means assign 'True' if the optional argument is specified
+    # on the command line, so the default for store_true is actually false
+    parser.add_argument("-v", "--verbose", help="Print verbose output", action='store_true')
+    # parse the arguments
+    args = parser.parse_args()
 
-fibonacci_number = a
+###------ function to calculate the Fibonacci number
+def fib():
+    #initialize two integers
+    a,b = 0,1
+    for i in range(int(args.position)):
+        a,b = b, a+b
 
-if args.verbose:
-    print(f"The Fibonacci number for {args.position} is {fibonacci_number}.")
-else:
-    print(fibonacci_number)
+    fibonacci_number = a
+
+###------ function to print the output
+def print_output():
+    if args.verbose:
+        print(f"The Fibonacci number for {args.position} is {fibonacci_number}.")
+    else:
+        print(fibonacci_number)
+
+
+###------ define the main() function
+def main():
+
+
+    # set the environment for this script
+    # is this main (i.e., a standalone Python script), or
+    # is this a Python module being called by another script
+    if __name__ == '__main__':
+        main()
+
